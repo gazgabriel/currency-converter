352e1b913572de213562455b52dd0052783da7f9
Gabriel Tavares <gazsmt@gmail.com>
2020-05-02 14:26:26 -0300

    Add Search feature

--- currency-converter/ViewController/SelectCurrencyViewController.swift
+++ currency-converter/ViewController/SelectCurrencyViewController.swift
@@ -13,9 +13,10 @@ protocol SelectCurrencyViewControllerDelegate: AnyObject {
     func updateConvert(_ prefix: String)
 }
 
-class SelectCurrencyViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class SelectCurrencyViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var selectCurrency: UIPickerView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var currencies: [String:String]?
     var segueOrigin: String?
@@ -27,6 +28,7 @@ class SelectCurrencyViewController: UIViewController, UIPickerViewDelegate, UIPi
         super.viewDidLoad()
         selectCurrency.dataSource = self
         selectCurrency.delegate = self
+        searchBar.delegate = self
         
         self.viewModel = SelectCurrencyViewModel(currencies: currencies!)
     }
@@ -36,21 +38,26 @@ class SelectCurrencyViewController: UIViewController, UIPickerViewDelegate, UIPi
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return (viewModel?.currenciesList.count)!
+        return (viewModel?.count())!
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return viewModel?.currenciesList[row]
+        return viewModel?.titleAtIndex(index: row)
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if segueOrigin == "ActualCurrencySet" {
-            self.delegate?.updateActual(String((viewModel?.currenciesList[row].prefix(3))!))
+            self.delegate?.updateActual((viewModel?.prefixAtIndex(index: row))!)
         }
         
         if segueOrigin == "ConvertCurrencySet" {
-            self.delegate?.updateConvert(String((viewModel?.currenciesList[row].prefix(3))!))
+            self.delegate?.updateConvert((viewModel?.prefixAtIndex(index: row))!)
         }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.viewModel?.currenciesByTerm(term: searchText)
+        self.selectCurrency.reloadAllComponents()
     }
     
 }
